4a5a81d40671b35ceb8cc63a2a345b3891c676d4
Shane Larson <shane@grizzlypeaksoftware.com>
2019-12-30 18:38:58 -0900

    Make bot programmable, fix keyboard controller

--- bot.py
+++ bot.py
@@ -3,26 +3,69 @@ import keyboard
 
 tankbot.Init()
 
-while True:
-	if keyboard.is_pressed("s"):
-		tankbot.Stop()
-	if keyboard.is_pressed("up"):
-		tankbot.Forward()
-	if keyboard.is_pressed("down"):
-		tankbot.Reverse()
-	if keyboard.is_pressed("right"):
-		tankbot.Right()
-	if keyboard.is_pressed("left"):
-		tankbot.Left()	
-	if keyboard.is_pressed("1"):
-		tankbot.No()	
-	if keyboard.is_pressed("2"):
-		tankbot.Yes()	
-	if keyboard.is_pressed("3"):
-		tankbot.Welcome()	
 
-	if keyboard.is_pressed("q"):
+import time as _time
+
+recorded = []
+recording_started = False
+
+def ControlSwitch(key, event):
+	if key == "s":
+		tankbot.Stop()
+	if key == "up":
+		tankbot.Forward()
+	if key == "down":
+		tankbot.Reverse()
+	if key == "right":
+		tankbot.Right()
+	if key == "left":
+		tankbot.Left()	
+	if key == "1":
+		tankbot.No()	
+	if key == "2":
+		tankbot.Yes()	
+	if key == "3":
+		tankbot.Welcome()	
+	if key == "f1":
+		keyboard.start_recording()
+		recording_started = True
+	if key == "f2":
+		try:
+			if recording_started == True:
+				recording_started = False
+				Playback(keyboard.stop_recording())
+			else:
+				Playback(recorded)
+		except Exception as inst:
+			print(type(inst))    # the exception instance
+			print(inst.args)     # arguments stored in .args
+			print(inst)          # __str__ allows args to be printed directly,
+	if key == "q"):
 		tankbot.Stop()
 		break
+
+def Playback(rec):
+	last_time = None    
+	global recorded
+	recorded = rec
+
+	for event in rec:
+		if last_time is not None:
+			_time.sleep(event.time - last_time)
+		last_time = event.time
+		key =  event.scan_code or event.name
+		print(event.name)
+
+		ControlSwitch(key, event)
+
+
+while True:
+
+	key = keyboard.read_key()
+	event = keyboard.read_event()
+
+	print(key)
+
+	ControlSwitch(key, event)
 	
 tankbot.Close()
